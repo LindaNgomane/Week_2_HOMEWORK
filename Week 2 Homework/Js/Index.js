669496076ae3c1ed3eef5e45d5cc68adf278617f
LindaNgomane <125291929+LindaNgomane@users.noreply.github.com>
2023-05-08 15:42:52 +0200

    Add files via upload

---
+++ Week 2 Homework/Js/Index.js
@@ -0,0 +1,53 @@
+console.log("Hello World")
+
+var firstName = "Linda"
+var surname = "Ngomane"
+var age = 18
+const subject = "development"
+
+let answer = "Hello, my name is " + firstName + " " + surname + ", I am " + age + " years old and I am studying " + subject
+
+console.log(answer)
+
+
+let numOne = 12
+let numTwo = 23
+let numThree = 49
+let numFour= 25
+
+let sum = numOne + numTwo / numThree * numFour
+
+console.log(sum)
+
+
+let timmy = 60
+let susan = 40
+let TMS = susan * 26/100
+let TMT = timmy * 15/100
+let value = TMS + TMT
+
+let TM = "The Tax Man will claim " + value + " From Timmy and Susan"
+
+console.log(TM)
+
+
+let john = 70
+let tim = 60
+let alfe = 80
+let gwen = 90
+
+
+john = john + 7
+tim = tim + 7
+alfe = alfe +7
+gwen = gwen + 7
+
+let marks = (john + tim + alfe + gwen) / 4
+let marksUP = (john + tim + alfe + gwen) / 4
+
+let classAvg = "The students average is: " + marks
+let updatedAVG = "The students updated average is: " + marksUP
+
+console.log(classAvg)
+console.log(updatedAVG)
+
